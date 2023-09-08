429e95a8cb14e5e1bbeb83470eefe31286aaacc5
ManelRosPuig <manel.rospuig@gmail.com>
2023-09-08 08:26:16 +0200

    Strong typed language

---
+++ test.js
@@ -0,0 +1,5 @@
+let fname = 'test';
+console.log(fname);
+
+fname = 8;
+console.log(fname);
---
+++ test.ts
@@ -0,0 +1,5 @@
+let fname = 'test';
+console.log(fname);
+
+fname = 8; // Error! Type '8' is not assignable to type 'string'.
+console.log(fname);
