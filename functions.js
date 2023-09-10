59c65d6e2a2c41caa0c7251784d4829f166262fc
ManelRosPuig <manel.rospuig@gmail.com>
2023-09-10 12:08:31 +0200

    Function types

--- functions.js
+++ functions.js
@@ -1,11 +1,19 @@
 "use strict";
+// Regular function
 function add(a, b) {
     return a + b;
 }
 console.log(add(1, 2));
+// String interpolation
 function printMe(name, age) {
     console.log(`My name is ${name}. I am ${age} years old.`);
 }
 printMe("John", 20);
+// Inline function
 const multiply = (a, b) => a * b;
 console.log(multiply(2, 3));
+// Default parameter
+function add2(a, b = 1) {
+    return a + b;
+}
+console.log(add2(1, 2));
--- functions.ts
+++ functions.ts
@@ -1,15 +1,25 @@
+// Regular function
 function add(a: number, b: number): number {
   return a + b;
 }
 
 console.log(add(1, 2));
 
+// String interpolation
 function printMe(name: string, age: number): void {
   console.log(`My name is ${name}. I am ${age} years old.`);
 }
 
 printMe("John", 20);
 
+// Inline function
 const multiply = (a: number, b: number): number => a * b;
 
 console.log(multiply(2, 3));
+
+// Default parameter
+function add2(a: number, b: number = 1): number {
+  return a + b;
+}
+
+console.log(add2(1, 2));
