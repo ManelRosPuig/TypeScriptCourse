5c5fa6d2001962c277cda6c5266d27eb1d31577a
ManelRosPuig <manel.rospuig@gmail.com>
2023-09-10 11:49:16 +0200

    Other datatypes and objects

--- test.js
+++ test.js
@@ -1,5 +1,36 @@
 "use strict";
 let user;
-user = 'john';
+user = 'John';
 user = user.toUpperCase();
-console.log(user);
+let age;
+age = 30;
+let isMarried;
+isMarried = false;
+let hobbies;
+hobbies = ['Sports', 'Cooking'];
+let person;
+person = {
+    name: 'John',
+    age: 30,
+    isMarried: false,
+    hobbies: ['Sports', 'Cooking']
+};
+let people;
+people = [
+    {
+        name: 'John',
+        age: 30,
+        isMarried: false,
+        hobbies: ['Sports', 'Cooking']
+    },
+    {
+        name: 'Jane',
+        age: 30,
+        isMarried: false,
+        hobbies: ['Sports', 'Cooking']
+    }
+];
+console.log(people[0].name);
+console.log(people[0].age);
+console.log(people[0].isMarried);
+console.log(people[0].hobbies);
--- test.ts
+++ test.ts
@@ -2,4 +2,52 @@ let user : string;
 user = 'John';
 user = user.toUpperCase();
 
-console.log(user);
+let age : number;
+age = 30;
+
+let isMarried : boolean;
+isMarried = false;
+
+let hobbies : string[];
+hobbies = ['Sports', 'Cooking'];
+
+let person : {
+    name: string;
+    age: number;
+    isMarried: boolean;
+    hobbies: string[];
+};
+
+person = {
+    name: 'John',
+    age: 30,
+    isMarried: false,
+    hobbies: ['Sports', 'Cooking']
+};
+
+let people : {
+    name: string;
+    age: number;
+    isMarried: boolean;
+    hobbies: string[];
+}[];
+
+people = [
+    {
+        name: 'John',
+        age: 30,
+        isMarried: false,
+        hobbies: ['Sports', 'Cooking']
+    },
+    {
+        name: 'Jane',
+        age: 30,
+        isMarried: false,
+        hobbies: ['Sports', 'Cooking']
+    }
+];
+
+console.log(people[0].name);
+console.log(people[0].age);
+console.log(people[0].isMarried);
+console.log(people[0].hobbies);
